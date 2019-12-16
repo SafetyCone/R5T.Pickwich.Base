f7eb7d2a8d3383bfd8aa641cf02603844114d4ed
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:10:31 -0800

    Added Visual Studio project file path provider service definition interface type.

---
+++ source/R5T.Pickwich.Base/Code/Services/Definitions/IVisualStudioProjectFilePathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Pickwich
+{
+    public interface IVisualStudioProjectFilePathProvider
+    {
+        string GetVisualStudioProjectFilePath(string projectDirectoryPath, string projectName);
+    }
+}
