64ca25c246521a55c7e10da022a129df772bda1a
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:09:40 -0800

    Added Visual Studio project file operator service definition interface type.

---
+++ source/R5T.Pickwich.Base/Code/Services/Definitions/IVisualStudioProjectFileOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Pickwich.Types;
+
+
+namespace R5T.Pickwich
+{
+    public interface IVisualStudioProjectFileOperator
+    {
+        string CreateProjectFile(ProjectType projectType, string projectDirectoryPath, string projectName);
+    }
+}
